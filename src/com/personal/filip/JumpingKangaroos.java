e3faf75596cc6ef284ca713a55833b244cfc13cc
filipchromy <Filip.Chromy99@gmail.com>
2022-01-27 00:33:43 +0100

    BetweenTwoSets problem solved. BirthdayChocolateBar problem solved. BreakingBestAndWorstRecords problem solved. JumpingKangaroos problem solved. -- more like mathematical problem, solved in place.
    27/01/2022

---
+++ src/com/personal/filip/JumpingKangaroos.java
@@ -0,0 +1,8 @@
+package com.personal.filip;
+
+public class JumpingKangaroos {
+    public static String kangaroo(int x1, int v1, int x2, int v2) {
+        // Solved in place.
+        return "0";
+    }
+}
